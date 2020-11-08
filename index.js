6d634714c235a95ce824336457135a4548769af0
백호동 <ghehd2311@gmail.com>
2020-11-08 11:14:18 +0900

    mongoose,dotenv install && mongoDB connect

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// mongoose 연결
+const mongoose = require('mongoose');
+
+//.env로 몽고 커넥트 주소 숨김
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
