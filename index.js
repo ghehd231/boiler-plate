1dd0e5c58b61a004f05e2760a37a23a77ebcec68
백호동 <ghehd2311@gmail.com>
2020-11-08 11:50:31 +0900

    bodyparser && 라우터 설정

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+// x-www-form-urlencoded 분석해 가져오기
+app.use(bodyParser.urlencoded({ extended: true }));
+// json타입 분석해 가져오기
+app.use(bodyParser.json());
 
 // mongoose 연결
 const mongoose = require('mongoose');
@@ -20,5 +27,18 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// 회원가입 라우터
+app.post('/register', (req, res) => {
+  // 회원가입 시 필요한 정보들을 client에서 가져오면 DB에 넣어줌
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
