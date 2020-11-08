79165228e1167e55680a0b982432a27c624964f0
백호동 <ghehd2311@gmail.com>
2020-11-08 10:58:45 +0900

    npm init && install express

---
+++ index.js
@@ -0,0 +1,8 @@
+// express 연결
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
